6c42d95db98dccf104b83df073d9352613c5ddcd
Nawamin Sukhothai <minsk783@gmail.com>
2021-06-29 17:21:05 +0700

    Create strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bdafe8fb88958e5f25cf49b0792859c4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ec9dcf5b-dd95-4f88-99cb-e70e6d28fad5'
+};
